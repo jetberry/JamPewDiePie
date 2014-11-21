b6d5a057c8d6d8801882efbaf911b8d9574135b4
Andrey Boyarshinov <rustray@gmail.com>
2014-11-21 16:31:32 +0700

    replace Airplane class

---
+++ Classes/airplane/Character.h
@@ -0,0 +1,18 @@
+//
+//  Character.h
+//  jamPewDiePie
+//
+//  Created by rustray on 21.11.14.
+//
+//
+
+#ifndef __jamPewDiePie__Character__
+#define __jamPewDiePie__Character__
+
+#include "cocos2d.h"
+#include "cocos-ext.h"
+
+
+
+
+#endif /* defined(__jamPewDiePie__Character__) */
