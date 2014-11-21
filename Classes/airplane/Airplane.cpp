5859ccc98338ee09e051e23ad21d08a240398c01
Andrey Boyarshinov <rustray@gmail.com>
2014-11-21 23:56:41 +0700

    move Character

--- Classes/airplane/Airplane.cpp
+++ Classes/airplane/Airplane.cpp
@@ -12,6 +12,15 @@ bool Airplane::init()
 	wall->setPhysicsBody(PhysicsBody::createEdgeBox(innerRect.size, PhysicsMaterial(0.1f, 1, 0.0f)));
 	wall->setPosition(Vec2());
 	this->addChild(wall);
+    
+    auto bottom = Node::create();
+    PhysicsBody* bodyBottom = PhysicsBody::createBox(Size(1170, 10), PhysicsMaterial(0.1f, 1, 0.0f));
+    bodyBottom->setDynamic(false);
+    bottom->setPhysicsBody(bodyBottom);
+    bottom->setPositionX(wall->getPositionX());
+    bottom->setPositionY(-526/2 + 10);
+    this->addChild(bottom);
+    
 
 	auto sprite = Sprite::create("airplane/airplane.png");
 	sprite->setPosition(Vec2());
@@ -21,6 +30,8 @@ bool Airplane::init()
 	//character->setPosition(1000, 750);
 	helpers::setDesignPosEx(character, 1000, 750);
 	this->addChild(character);
+    character->createBody();
+    character->setBottom(bottom->getPhysicsBody());
 
 	return true;
 }
