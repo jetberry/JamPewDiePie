3ffd651a406b6db7af7c9f6cb2cd78b8d3216bf7
osip-rubinstein <osip.rubinsteing@gmail.com>
2014-11-24 05:36:53 +0700

    Добавился очкарик.

---
+++ Classes/passenger/Nerd.cpp
@@ -0,0 +1,30 @@
+#include "Nerd.h"
+
+USING_NS_CC;
+
+bool Nerd::init()
+{
+	if (!Sprite::init())
+		return false;
+
+	scheduleUpdate();
+	setZOrder(-5);
+
+	this->setTexture("airplane/people/passengers/0003/seating.png");
+	this->setPosition(Vec2(340, 40));
+
+	timeToDead = 300;
+
+	return true;
+}
+
+void Nerd::update(float dt)
+{
+	if (timeToDead)
+	{
+		timeToDead--;
+		return;
+	}
+
+	this->setTexture("airplane/people/passengers/0003/dead.png");
+}
---
+++ Classes/passenger/Nerd.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "cocos2d.h"
+
+class Nerd : public cocos2d::Sprite
+{
+public:
+
+	CREATE_FUNC(Nerd);
+
+protected:
+
+	virtual bool init() override;
+	virtual void update(float dt) override;
+
+	int timeToDead;
+
+};
